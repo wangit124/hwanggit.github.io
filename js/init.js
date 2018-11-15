b1c965379c03c4bb3b00715dbe14b280e8cdd9cf
howard wang <luhao.wang2016@gmail.com>
2018-11-14 22:30:00 -0800

    changed color theme

--- js/init.js
+++ js/init.js
@@ -1,11 +1,11 @@
 function openSideMenu() {
 	document.getElementById('side-menu').style.width='250px';
-	document.getElementById('main').style.marginLeft='250px';
+	//document.getElementById('main').style.marginLeft='250px';
 }
 
 function closeSideMenu() {
 	document.getElementById('side-menu').style.width='0';
-	document.getElementById('main').style.marginLeft='0';
+	//document.getElementById('main').style.marginLeft='0';
 }
 
 var slider={
