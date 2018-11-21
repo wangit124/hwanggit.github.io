50352df6a25d7f110a75b398716273fc5eb08489
howard wang <luhao.wang2016@gmail.com>
2018-11-21 15:23:37 -0800

    added work section

--- js/init.js
+++ js/init.js
@@ -8,14 +8,10 @@ function closeSideMenu() {
 	document.getElementById('side-menu').style.width='0';
 	//document.getElementById('main').style.marginLeft='0';
 }
+function out(arg){ document.getElementById("debug").innerHTML=arg;}
 
 //CAROUSEL
-
-//Helper function(s)
-function degToRad(input) { return input*(Math.PI/100);}
-
-function out(arg){ document.getElementById("debug").innerHTML=arg;}
-var Carousel = function (){
+/*var Carousel = function (){
 	//definitions and init
 	var me=this;
 
@@ -23,11 +19,11 @@ var Carousel = function (){
 	var carouselH = 300;
 	var itemW = 100;
 	var itemH = 200;
-	var noOfItemsToAdd = 3;
+	var noOfItemsToAdd = 1;
 
 	//Working variables
 	var carousel =document.getElementById("rotating-carousel");
-	var itemContainer=carousel.getElementByClassName("item-container");
+	var itemContainer=carousel.getElementByClassName("item-container")[0];
 	var items=[];
 	var deg=0;
 	var rangeX=carouselW;
@@ -48,48 +44,124 @@ var Carousel = function (){
 	    
 	    var item=document.createElement("div");
 	    item.classList.add("item");
-	    carousel.appendChild(item);
+	    itemContainer.appendChild(item);
 
 	    var itemObject = {
-		item:item
+		carouselItem:item
 	    };
+
+	    items.push(itemObject);
 	}
 	    
 
 	function animate() {
 	    deg+=0.01;
-	    var sin = Math.sin(degToRad(deg)*rangeX);
-	    requestAnimationFrame(animate);
-	}
+	    var sin = 0.5+ (Math.sin(deg)*0.5);
+	    out(sin);
 
+	    for (var i=0;i<items.length;i++) {
+		var itemObject=items[i];
+		var posX=sin*rangeX;
+		itemObject.carouselItem.style.left=postX="px";
+	    }
+
+	    window.requestAnimationFrame(animate);
+	}
+}
+//Helper function(s)
+function degToRad(input) { return input*(Math.PI/100);}
+*/
+
+/*$(function() {
+
+
+   var defaultCss = {
+	width: 240,
+	height: 150,
+	marginTop: 50,
+	marginRight: 25,
+	marginLeft: 25,
+	opacity: 0.5
+   };	
+
+   var selectedCss = {
+	width: 400,
+	height: 260,
+	marginTop: 0,
+	marginRight: -25,
+	marginLeft: -25,
+	opacity: 1
+   };
+
+   var aniOpts = {
+	queue: false,
+	duration: 1000,
+	easing: 'elastic'
+   };
+
+   var $car = $('#carousel');
+
+   for (var a = 0; a<3; a++) {
+	$car.prepend('<div/>');
+   }
+    
+   for (var a = 0; a<3;a++) {
+	$car.append('<div/>');
+   }
+   $car.find('img').css('zIndex', 1).css(defaultCss);
+   $car.find('img').eq(0).css('zIndex',2).css(selectedCss);
+   $car.carouFredSel({
+	circular:false,
+	infinite:false,
+	width:'100%', 
+	height: 250, 
+	items: 7, 
+	prev: '#prev', 
+	next: '#next', 
+	auto: false,
+	scroll: {
+		items: 1,
+		duration: 1000,
+		easing: 'elastic',
+		onBefore: function(data) {
+			data.items.old.eq(3).find('img').css('zIndex', 1).animate(defaultCss,aniOpts);
+			data.items.visible.eq(3).find('img').css('zIndex', 2).animate(selectedCss, aniOpts);
+		}
+	}
+   });
+});
+*/
 //implement draggable side nav
 //implement hover slide for personal slider
 
 //PERSONAL SLIDE
 var slider={
-	
+
 	//define slide variables
 	el: {
-	    slider: $("#slider"),
-	    allSlides: $(".slide")
+		slider: $("#slider"),
+		allSlides: $(".slide")
 	},
 
 	//initial function, scroll slider
 	init: function() {
-	    this.el.slider.on("scroll",function(event) {
-		slider.moveSlidePosition(event);
-	    });
+		this.el.slider.on("scroll",function(event) {
+			slider.moveSlidePosition(event);
+		});
 	},
 
 	//helper function for scroll
 	moveSlidePosition: function(event) {
-	     this.el.allSlides.css({
+		this.el.allSlides.css({
 		"background-position": $(event.target).scrollLeft()/6-100+ "px 0"
-	     });
+		});
 	}
-};
+}
 
-//initialize 
 slider.init();
 
-
+//initialize 
+/*function pageLoadInit(){
+var myCarousel= new Carousel();
+}
+*/
