fe29c1e803eaa14684baa97d108eeed2bd95aa4a
Howard Wang   (王陆豪) <luw055@ucsd.edu>
2019-01-03 01:57:39 -0800

    added Education section and fixed flipper modules

---
+++ init.js
@@ -0,0 +1,12 @@
+//SIDE NAV//
+function openSideMenu() {
+	document.getElementById('side-menu').style.height='180px';
+	document.getElementById('side-menu').style.paddingTop='8px';
+	//document.getElementById('main').style.marginLeft='250px';
+}
+
+function closeSideMenu() {
+	document.getElementById('side-menu').style.height='0';
+	document.getElementById('side-menu').style.paddingTop='0px';
+	//document.getElementById('main').style.marginLeft='0';
+}
