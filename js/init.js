bd01b5174d07f0fab1838616e89d71c959e24fd7
Howard Wang <luw055@ucsd.edu>
2019-03-19 20:01:56 -0700

    Revamped website

--- js/init.js
+++ js/init.js
@@ -1,12 +1,19 @@
-//SIDE NAV//
-function openSideMenu() {
-	document.getElementById('side-menu').style.height='210px';
-	document.getElementById('side-menu').style.paddingTop='8px';
-	//document.getElementById('main').style.marginLeft='250px';
-}
 
-function closeSideMenu() {
-	document.getElementById('side-menu').style.height='0';
-	document.getElementById('side-menu').style.paddingTop='0px';
-	//document.getElementById('main').style.marginLeft='0';
-}
+
+$(window).on("load",function() {
+	$(window).scroll(function() {
+	  var windowBottom = $(this).scrollTop() + $(this).innerHeight();
+	  $(".fade").each(function() {
+		/* Check the location of each desired element */
+		var objectBottom = $(this).offset().top + $(this).outerHeight();
+		
+		/* If the element is completely within bounds of the window, fade it in */
+		if (objectBottom < (windowBottom-80)) { //object comes into view (scrolling down)
+		  if ($(this).css("opacity")==0) { $(this).animate({ opacity: 1 }, { duration: 100, queue: false });
+			$(this).animate({ "margin-top": "10px" }, { duration: 500, queue: false });}
+		  }
+	  });
+	}).scroll(); //invoke scroll-handler on page-load
+  });
+
+  
