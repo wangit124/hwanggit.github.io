8574238c01c978aac2a2df835ce10394447530e6
hwanggit <luw055@ucsd.edu>
2019-07-18 22:56:30 -0700

    added cloud animations

--- js/animate.js
+++ js/animate.js
@@ -4,15 +4,32 @@ $(document).ready(function () {
         $('#globe-animate').css('height', $(window).height() - $('#topnav').height() * 2)
         $('#globe-animate').css('padding-top', $('#topnav').height())
         $('#globe-animate svg').css('margin-top', $(window).height() * 0.6)
+        $('#cloud-header-2').css('top',  $(window).height() * 0.25)
+        $('#cloud-header-2').css('left', '72%')
+        $('#cloud-header-3').css('top',  $(window).height() * 0.30)
+        $('#cloud-header-3').css('left', '68%')
+        $('#cloud-header').css('top',  $(window).height() * 0.20)
+        $('#cloud-header').css('left', '35%')
     }
     else {
-        $('#globe-animate').css('height', $(document).height() - $('#topnav').height())
+        $('#globe-animate').css('height', $(window).height() - $('#topnav').height())
         $('#globe-animate').css('padding-top', $('#topnav').height())
-        $('#globe-animate svg').css('margin-top', $(window).height() * 0.5)
+        $('#globe-animate svg').css('margin-top', $(window).height() * 0.6)
+        $('#cloud-header-2').css('top',  $(window).height() * 0.25)
+        $('#cloud-header-2').css('left', '50%')
+        $('#cloud-header-3').css('top',  $(window).height() * 0.28)
+        $('#cloud-header-3').css('left', '70%')
+        $('#cloud-header').css('top',  $(window).height() * 0.16)
+        $('#cloud-header').css('left', '28%')
     }
 
+    // Set dialog box height and width to be same as text
+    $('#dialog-box').css('height', $('#dialog-box p').height() * 1.5)
+    $('#dialog-box').css('width', $('#dialog-box p').width() * 1.2)
+    $('#dialog-box p').css('padding-top', $('#dialog-box p').height() * 0.25)
+    
     // Set top margin of sprout animation
-    $('#sprout-animate').css('margin-top', parseInt($('#globe-animate svg').css('margin-top')) - parseInt($('#sprout-animate').height()))
+    $('#sprout-animate').css('margin-top', parseInt($('#globe-animate svg').css('margin-top')) - parseInt($('#sprout-animate').height()) - parseInt($('#dialog-box').height()))
 });
 
 $(window).resize(function () {
@@ -20,14 +37,26 @@ $(window).resize(function () {
         $('#globe-animate').css('height', $(window).height() - $('#topnav').height() * 2)
         $('#globe-animate').css('padding-top', $('#topnav').height())
         $('#globe-animate svg').css('margin-top', $(window).height() * 0.6)
+        $('#cloud-header-2').css('top',  $(window).height() * 0.25)
+        $('#cloud-header-2').css('left', '72%')
+        $('#cloud-header-3').css('top',  $(window).height() * 0.30)
+        $('#cloud-header-3').css('left', '68%')
+        $('#cloud-header').css('top',  $(window).height() * 0.20)
+        $('#cloud-header').css('left', '35%')
     }
     else {
-        $('#globe-animate').css('height', $(document).height() - $('#topnav').height())
+        $('#globe-animate').css('height', $(window).height() - $('#topnav').height())
         $('#globe-animate').css('padding-top', $('#topnav').height())
-        $('#globe-animate svg').css('margin-top', $(window).height() * 0.5)
+        $('#globe-animate svg').css('margin-top', $(window).height() * 0.6)
+        $('#cloud-header-2').css('top',  $(window).height() * 0.25)
+        $('#cloud-header-2').css('left', '50%')
+        $('#cloud-header-3').css('top',  $(window).height() * 0.28)
+        $('#cloud-header-3').css('left', '70%')
+        $('#cloud-header').css('top',  $(window).height() * 0.16)
+        $('#cloud-header').css('left', '28%')
     }
-    $('#sprout-animate').css('margin-top', parseInt($('#globe-animate svg').css('margin-top')) - parseInt($('#sprout-animate').height()))
-});
+    
+    $('#sprout-animate').css('margin-top', parseInt($('#globe-animate svg').css('margin-top')) - parseInt($('#sprout-animate').height()) - parseInt($('#dialog-box').height()))});
 
 // Animate walking cycle
 $(function(){
@@ -51,7 +80,7 @@ $(function(){
         counter++
         
         // Once counter reaches threshold, stop
-        if (counter === 56) clearInterval(duration)
+      //  if (counter === 56) clearInterval(duration)
 
         // Reset currId
         if (currId === 9) {
@@ -59,6 +88,39 @@ $(function(){
         }
     }, 100);   
 
+});
+
+// Function move left and right
+function moveLeftandRight(id, start, end, duration) {
+    $( "#" + id).animate({
+        left: start+'%',
+        }, duration, function() {
+    });
+    $( "#" + id).animate({
+        left: end+'%',
+        }, duration, function() {
+    });
+}
+
+// Animate clouds
+$(function() {
+    // Repeatedly move cloud left and right
+    moveLeftandRight("cloud-header-3", 65, 70, 15000) 
+    window.setInterval(function(){
+        moveLeftandRight("cloud-header-3", 65, 70, 15000) 
+    }, 30000);   
+
+    // Repeatedly move cloud left and right
+    moveLeftandRight("cloud-header-2", 55, 45, 8000) 
+    window.setInterval(function(){
+        moveLeftandRight("cloud-header-2", 55, 45, 8000) 
+    }, 16000); 
+
+    // Repeatedly move cloud left and right
+    moveLeftandRight("cloud-header", 40, 30, 10000) 
+    window.setInterval(function(){
+        moveLeftandRight("cloud-header", 40, 30, 10000) 
+    }, 20000); 
 });
 
 // Rotation Animation plugin (cross-browser)
@@ -108,14 +170,6 @@ $.fn.animateRotate = function (endAngle, options, startAngle) {
     });
 };
 
-// Rotate globe from -90 to 40 degrees
-$('#globe-animate svg').animateRotate(90, {duration: 0, 
-    complete: function() { 
-        $('#globe-animate svg').animateRotate(60, {duration: 5700, 
-            complete: function() { 
-                
-            }
-        });        
-    }
-});
+$('#globe-animate svg').animateRotate(-360, {duration: 40000});  
+
 
